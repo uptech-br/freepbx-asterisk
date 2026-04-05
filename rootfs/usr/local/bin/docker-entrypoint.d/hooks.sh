2593314ce3eddc8260c9d103473494cd589ffb47
Mikaell Duarte <root@uptech.com.br>
2026-04-05 14:36:40 -0300

    feat(nftables): injeta variáveis de ambiente no hook nftables

--- rootfs/usr/local/bin/docker-entrypoint.d/hooks.sh
+++ rootfs/usr/local/bin/docker-entrypoint.d/hooks.sh
@@ -1,30 +1,39 @@
 # shellcheck shell=bash
 
-load_nftables_configs() {
+function load_nftables_configs() {
     local hooks_dir="${1:?hooks_dir required}"
     local config_dir="${hooks_dir}/nftables.d"
+    
+    local -a nft_args=()
+    local ipvlan_if="$(ipvlan_iface)"
+
+    if [[ -n "${ipvlan_if}" ]]; then
+        nft_args+=(--define "ipvlan_if=${ipvlan_if}")
+    fi
 
     log "Loading nftables basic rules..."
-    nft -f /etc/nftables.nft
+    nft "${nft_args[@]}" -f /etc/nftables.nft
 
     log "Loading nftables configurations from ${config_dir}"
     if [[ -d "${config_dir}" ]]; then
         local file
+
         for file in "${config_dir}"/*.nft; do
             [[ -f "${file}" ]] || continue
             log "Applying nftables configuration: ${file}"
-            nft -f "${file}"
+            nft  "${nft_args[@]}" -f "${file}"
         done
     fi
 }
 
-run_bash_scripts() {
+function run_bash_scripts() {
     local hooks_dir="${1:?hooks_dir required}"
     local scripts_dir="${hooks_dir}/scripts.d"
 
     log "Executing bash scripts in ${scripts_dir}"
     if [[ -d "${scripts_dir}" ]]; then
         local script
+
         for script in "${scripts_dir}"/*.sh; do
             [[ -f "${script}" ]] || continue
             log "Running script: ${script}"
